a21e2e25d7a439a3b2523cda738a2114577a0c48
Launcher ESP32 <dev@launcher-esp32>
2026-06-12 06:20:09 +0800

    add: board init code

--- main/app-main.cpp
+++ main/app-main.cpp
@@ -1,25 +1,42 @@
 #include "sdkconfig.h"
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <nvs_flash.h>
-#include <esp_log.h>
 
+#include "board/m5stack/tab5.hpp"
 #include "wrapper/freertos.hpp"
+#include "wrapper/logger.hpp"
 
-static const char *TAG = "launcher-esp32";
+using namespace wrapper;
 
-extern "C" void app_main(void)
-{
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+Logger lmain("app-main");
 
-    ESP_LOGI(TAG, "launcher-esp32 started");
+Task board_init(
+    "BoardInit",
+    [](void *) {
+      M5StackTab5 &board = M5StackTab5::GetInstance();
+      board.InitCoreBusAndIoExpander();
+      board.InitDisplay();
+      board.GetLvglPort().SetRotation(LV_DISPLAY_ROTATION_90);
+      board.InitAudio();
 
-    while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+      board.TestKeyboard();
+    },
+    nullptr, 8192, 5);
+
+extern "C" void app_main(void) {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  lmain.Info("Starting board initialization task...");
+  if (!board_init.Create()) {
+    lmain.Error("Failed to create board initialization task");
+    return;
+  }
 }
